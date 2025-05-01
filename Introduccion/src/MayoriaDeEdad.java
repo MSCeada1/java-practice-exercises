ec35cd7f8fa9fa958abf5098d65e04f7ca2a4baf
MSCeada1 <195111856+MSCeada1@users.noreply.github.com>
2025-05-01 22:26:00 +0200

    primer commit

---
+++ Introduccion/src/MayoriaDeEdad.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+//El objetivo de este ejercicio es averiguar si el usuario es mayor de edad.
+public class MayoriaDeEdad {
+
+    static Scanner entrada = new Scanner(System.in);
+    static final int EDAD_MAXIMA = 18;
+    static final int YEAR_ACTUAL = 2025;
+
+    //El metodo yearNacimiento() se encarga de pedir el año en el que el usuario nació.
+    public static short yearNacimiento() {
+        short year;
+        System.out.println("¿En que año naciste?");
+        year = entrada.nextShort();
+        return year;
+    }
+
+    //El metodo mayoria de edad se encarga de averiguar si el usuario es mayor de edad o no.
+    public static void mayoriaDeEdad() {
+        if (YEAR_ACTUAL - yearNacimiento() < EDAD_MAXIMA) {
+            System.out.println("Eres menor de edad");
+        } else {
+            System.out.println("Eres mayor de edad");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        mayoriaDeEdad();
+    }
+}
---
+++ Introduccion/src/MiPrimeraClase.java
@@ -0,0 +1,10 @@
+public class MiPrimeraClase {
+
+    public static void main(String[] args) {
+        mensaje("Hola mundo desde java");
+    }
+
+    public static void mensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
